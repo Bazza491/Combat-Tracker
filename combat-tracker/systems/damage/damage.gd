b5e6a0fe17f6fb867ad43c23f3c222a48e544730
Bazza491 <85243432+Bazza491@users.noreply.github.com>
2026-05-16 21:34:07 +0930

    importing resources from another project
    Dice icons by Itomi Bhaa from https://itomi.itch.io/rpg-dice-icons

---
+++ combat-tracker/systems/damage/damage.gd
@@ -0,0 +1,50 @@
+extends Object
+class_name Damage
+
+enum Type {
+	ACID,
+	BLUDGEONING,
+	COLD,
+	FIRE,
+	FORCE,
+	LIGHTNING,
+	NECROTIC,
+	PIERCING,
+	POISON,
+	PSYCHIC,
+	RADIANT,
+	SLASHING,
+	THUNDER
+}
+
+const TYPE_TEXT := {
+	Type.ACID: "Acid",
+	Type.BLUDGEONING: "Bludgeoning",
+	Type.COLD: "Cold",
+	Type.FIRE: "Fire",
+	Type.FORCE: "Force",
+	Type.LIGHTNING: "Lightning",
+	Type.NECROTIC: "Necrotic",
+	Type.PIERCING: "Piercing",
+	Type.POISON: "Poison",
+	Type.PSYCHIC: "Psychic",
+	Type.RADIANT: "Radiant",
+	Type.SLASHING: "Slashing",
+	Type.THUNDER: "Thunder",
+}
+
+const DAMAGE_TYPE_COLOR := {
+	Type.ACID: Color("#7FFF00"),         # bright toxic green
+	Type.BLUDGEONING: Color("#8B6F47"), # earthy brown
+	Type.COLD: Color("#7FDBFF"),         # icy blue
+	Type.FIRE: Color("#FF5A36"),         # orange-red
+	Type.FORCE: Color("#C080FF"),        # arcane purple
+	Type.LIGHTNING: Color("#FFE14A"),    # electric yellow
+	Type.NECROTIC: Color("#4B4453"),     # dark desaturated purple
+	Type.PIERCING: Color("db0b0bff"),     # steel grey
+	Type.POISON: Color("#6BDB3F"),       # venom green
+	Type.PSYCHIC: Color("#FF4FD8"),      # vivid magenta
+	Type.RADIANT: Color("#FFF4A3"),      # holy pale gold
+	Type.SLASHING: Color("#D64545"),     # blood red
+	Type.THUNDER: Color("#5DA9FF"),      # storm blue
+}
--- combat-tracker/systems/dice/dice.gd
+++ combat-tracker/systems/dice/dice.gd
@@ -1,2 +1,31 @@
 extends Object
 class_name Dice
+
+enum Type {
+	FLAT_DAMAGE,
+	D4,
+	D6,
+	D8,
+	D10,
+	D12,
+	D20
+}
+
+const TYPE_TEXT := {
+	Type.FLAT_DAMAGE: "Flat Damage",
+	Type.D4: "D4",
+	Type.D6: "D6",
+	Type.D8: "D8",
+	Type.D10: "D10",
+	Type.D12: "D12",
+	Type.D20: "D20"
+}
+
+const TYPE_TEXTURE: Dictionary[Type, Texture]= {
+	Type.D4: preload("res://systems/dice/icons/128x128/D4.png"),
+	Type.D6: preload("res://systems/dice/icons/128x128/D6.png"),
+	Type.D8: preload("res://systems/dice/icons/128x128/D8.png"),
+	Type.D10: preload("res://systems/dice/icons/128x128/D10.png"),
+	Type.D12: preload("res://systems/dice/icons/128x128/D12.png"),
+	Type.D20: preload("res://systems/dice/icons/128x128/D20.png"),
+}
