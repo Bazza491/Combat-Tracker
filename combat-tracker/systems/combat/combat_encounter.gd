9e3dc57e1bd2e6635362d367eb1d1904367bbef1
Bazza491 <85243432+Bazza491@users.noreply.github.com>
2026-05-20 20:07:02 +0930

    combat encounter class expanded with helper functions

--- combat-tracker/systems/combat/combat_encounter.gd
+++ combat-tracker/systems/combat/combat_encounter.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name CombatEncounter
 
-@export var entities: Dictionary[String, Entity] = {}
+@export var entities: Array[Entity] = []
 
 #@export var 
 
@@ -25,3 +25,53 @@ enum HealthDisplayMode {
 	PERCENT_INT,
 	PERCENT_FLOAT,
 }
+
+func sort_by_initiative() -> void:
+	entities.sort_custom(
+		func(a: Entity, b: Entity):
+			return a.initiative < b.initiative
+	)
+
+func sort_by_label() -> void:
+	entities.sort_custom(
+		func(a: Entity, b: Entity):
+			return a.get_label() < b.get_label()
+	)
+
+#func get_enemies() -> Array[Enemy]:
+	#return entities.filter(
+		#func(entity: Entity): 
+			#return entity is Enemy
+			#)
+
+#func get_players() -> Array[Player]:
+	#return entities.filter(
+		#func(entity: Entity): 
+			#return entity is Player
+			#)
+
+func find_label_or_null(search_label: String) -> Entity:
+	var matching_entity: Entity = null
+	for entity in entities:
+		if entity.get_label() == search_label:
+			matching_entity = entity
+	
+	return matching_entity
+
+func find_closest_label(search_label: String) -> Entity:
+	if entities.is_empty(): return null
+	var closest_entity: Entity = null
+	var dummy: Entity = Entity.new()
+	dummy.id = search_label + "!"
+	
+	entities.append(dummy)
+	sort_by_label()
+	
+	if entities.find(dummy) != 0:
+		closest_entity = entities[entities.find(dummy) - 1]
+	else:
+		closest_entity = entities[1]
+	
+	entities.remove_at(entities.find(dummy))
+	
+	return closest_entity
