8e41732ce6cd82de8ecd4fc7aa00a636fecbacb1
Bazza491 <85243432+Bazza491@users.noreply.github.com>
2026-05-21 00:39:04 +0930

    laying out design for text based combat tracker

--- combat-tracker/entities/windows/text_tracker/text_tracker.gd
+++ combat-tracker/entities/windows/text_tracker/text_tracker.gd
@@ -5,22 +5,26 @@ DM-Facing, built for mouse-free, keyboard-only control so that
 simple, quick commands like:
 	dmg 1 20 psych
 deals 20 psychic damage (accounting for resistances and 
-vulnerabilities) to enemy "1". 
+vulnerabilities) to enemy "1". (damage type optional in command)
 
 other command examples to set up:
 	dmg 3 4d12+6 psych
 		Will also roll damage
-	mstr s2 255hp 20ac res psych thunder fire vuln light rad bludg
-		Creates (and adds to list) monster name "s2", with 
-		255 hp, 	20 armour class, resistance to psychic, thunder 
+	enemy s2 255hp 20ac res psych thunder fire vuln light rad bludg
+		Creates (and adds to list) enemy with label "s2", with 
+		255 hp, 20 armour class, resistance to psychic, thunder 
 		and fire damage, and vulnerability to lightning radiant,
 		and bludgeoning damage
 		
 		hp, ac, etc. are order agnostic, but command is always 
 		first, then enemy name, then args
+	enemy bat x6 40hp
+		creates 6 bats (labels bat, bat2, bat3, etc.) with 40hp but all 
+		unspecified stats are defaulted
 	clr
-		clears all info
-	turn george pete frauline rupert
+		clears all info and resets the combat encounter to empty (without 
+		changing preferences/flags)
+	turns george pete frauline rupert
 		sets the turn order widget to the above list of names 
 		(no/default initiative count)
 	turn george 20
@@ -38,15 +42,62 @@ option in alphabetical order
 		query: String, 
 		options: Array[String]
 		)
+	instead of this, for now, use the current solution of adding the query to 
+	the options array, sorting the options array alphabetically, then choosing
+	the appropriate option before or after the search query in alphabetical 
+	order
 """
 extends Control
 
 @export var display_text: RichTextLabel
-@export var line_edit: LineEdit
+@export var terminal: LineEdit
+
+
+enum Command {
+	NONE,
+	DAMAGE,
+	HEAL,
+	ADD_ENEMY,
+	CLEAR,
+	SET_TURNS_LIST,
+	ADD_PLAYER_AT_INITIATIVE,
+	SKIP_TO_INITIATIVE
+}
+
+
+func _process_command(input: String) -> void:
+	var command: Command = _pick_command(input)
+	match command:
+		Command.NONE:
+			pass
+		Command.DAMAGE:
+			pass
+
 
 func _ready() -> void:
-	pass
-	# _clear_display()
-	# _display_tutorial_text()
-	# _connect_signals() 
-		# including line_edit.text_submitted
+	_clear_display()
+	_display_tutorial_text()
+	#_initialise_combat_encounter()
+	_connect_signals() 
+		# including terminal.text_submitted
+
+
+func _clear_display() -> void:
+	display_text.text = ""
+	_process_command("clear")
+
+
+func _display_tutorial_text() -> void:
+	display_text.text = \
+	"""
+	use command "enemy {name} x{amount} {hit points}hp" to get started
+	damage enemies with "dmg {name} {amount}"
+	"""
+
+
+func _connect_signals() -> void:
+	terminal.text_submitted.connect(_process_command)
+
+
+func _pick_command(input: String) -> Command:
+	return Command.NONE
